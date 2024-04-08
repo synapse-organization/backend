0d4897d57d89ac28624ed14d01a8ebd825b53d2b
Seyed Ali Kamali Pargoo <wwwwwq37@gmail.com>
2024-04-08 15:27:20 +0330

    feat: Changes image address for docker

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,5 +1,6 @@
 cd ..
 go mod vendor
-docker rmi backend:latest
-docker build . -t backend:latest -f deploy/Dockerfile
-docker save -o deploy/backend.tar backend:latest
+#docker rmi backend:latest
+docker build . -t irania9o/synapse_backend:latest -f deploy/Dockerfile
+docker push irania9o/synapse_backend:latest
+#docker save -o deploy/backend.tar backend:latest
