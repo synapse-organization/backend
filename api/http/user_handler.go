daeb53a641e248ab2bcce200c635a6791f0af891
mobinbr <mobinbr99@gmail.com>
2024-04-13 01:30:34 +0330

    Adds user profile

--- api/http/user_handler.go
+++ api/http/user_handler.go
@@ -20,26 +20,32 @@ type User struct {
 	Handler *modules.UserHandler
 }
 
-func (u User) ForgetPassword(c *gin.Context) {
+func (u User) SignUp(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(c, TimeOut)
 	defer cancel()
 
-	var user models.User
-	err := c.ShouldBindJSON(&user)
+	var data models.User
+
+	err := c.ShouldBindJSON(&data)
 	if err != nil {
 		log.GetLog().Errorf("Unable to bind json. error: %v", err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		c.JSON(400, gin.H{"error": "Unable to bind json"})
 		return
 	}
 
-	err = u.Handler.ForgetPassword(ctx, &user)
+	err = u.Handler.SignUp(ctx, &data)
 	if err != nil {
-		log.GetLog().Errorf("Unable to process forgetpassword. error: %v", err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		errValue := err.Error()
+		if !utils.IsCommonError(err) {
+			log.GetLog().Errorf("Unable to sign up. error: %v", err)
+			errValue = "Unable to sign up"
+		}
+
+		c.JSON(500, gin.H{"error": errValue})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	c.JSON(200, gin.H{"status": "ok"})
 	return
 }
 
@@ -73,31 +79,8 @@ func (u User) Login(c *gin.Context) {
 	return
 }
 
-func (u User) SignUp(c *gin.Context) {
-	ctx, cancel := context.WithTimeout(c, TimeOut)
-	defer cancel()
-
-	var data models.User
-
-	err := c.ShouldBindJSON(&data)
-	if err != nil {
-		log.GetLog().Errorf("Unable to bind json. error: %v", err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Unable to bind json"})
-		return
-	}
-
-	err = u.Handler.SignUp(ctx, &data)
-	if err != nil {
-		if !utils.IsCommonError(err) {
-			log.GetLog().Errorf("Unable to sign up. error: %v", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to sign up"})
-		}
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusCreated, gin.H{"status": "ok"})
-	return
+func (u User) GetUser(c *gin.Context) {
+	c.JSON(200, gin.H{"status": "ok"})
 }
 
 func (u User) VerifyEmail(c *gin.Context) {
@@ -118,6 +101,49 @@ func (u User) VerifyEmail(c *gin.Context) {
 	return
 }
 
-func (u User) GetUser(c *gin.Context) {
+func (u User) ForgetPassword(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(c, TimeOut)
+	defer cancel()
+
+	var user models.User
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		log.GetLog().Errorf("Unable to bind json. error: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+	err = u.Handler.ForgetPassword(ctx, &user)
+	if err != nil {
+		log.GetLog().Errorf("Unable to process forget-password. error: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	return
+}
+
+func (u User) UserProfile(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(c, TimeOut)
+	defer cancel()
+
+	incoming_token := c.Request.Header["Authorization"][0]
+
+	user, err := u.Handler.UserProfile(ctx, incoming_token)
+	if err != nil {
+		log.GetLog().Errorf("Unable to get user profile. error: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+		"first name": user.FirstName,
+		"last name": user.LastName,
+		"email": user.Email,
+		"phone": user.Phone,
+		"sex": user.Sex,
+	})
+	return
 }
--- internal/modules/user.go
+++ internal/modules/user.go
@@ -25,82 +25,6 @@ const (
 	passwordLength = 8
 )
 
-func (u UserHandler) ForgetPassword(ctx context.Context, user *models.User) error {
-	// Find the user based on email
-	foundUser, err := u.UserRepo.GetByEmail(ctx, user.Email)
-	if err != nil {
-		log.GetLog().Errorf("Email does not exist. error: %v", err)
-		return err
-	}
-
-	// Generate a random password
-	newPassword := utils.GenerateRandomPassword(passwordLength)
-
-	// Update user's password with the new random password
-	hashedPassword, err := utils.HashPassword(newPassword)
-	if err != nil {
-		log.GetLog().Errorf("Unable to hash password. error: %v", err)
-		return err
-	}
-	foundUser.Password = hashedPassword
-
-	// Save the updated password to the database
-	if err := u.UserRepo.UpdatePassword(ctx, foundUser.ID, foundUser.Password); err != nil {
-		log.GetLog().Errorf("Unable to update user's password. error: %v", err)
-		return err
-	}
-
-	// Send email with the new password to the user
-	emailBody := fmt.Sprintf(`Hello %s,<br><br>
-	A new password has been requested for your Barista account associated with %s.<br><br>
-
-	Here is your new password: <strong>%s</strong><br><br>
-
-	You can use your random generated password to login to your account.<br><br>
-
-	After logging in your account, you can reset your password in your profile section.<br><br>
-
-	Yours,<br>
-	The Synapse team`, foundUser.FirstName, foundUser.Email, newPassword)
-	err = utils.SendEmail(foundUser.Email, "Barista account recovery", emailBody)
-	if err != nil {
-		log.GetLog().Errorf("Unable to send email. error: %v", err)
-		return err
-	}
-
-	return nil
-}
-
-func (u UserHandler) Login(ctx context.Context, user *models.User) (string, string, error) {
-	foundUser, err := u.UserRepo.GetByEmail(ctx, user.Email)
-	if err != nil {
-		log.GetLog().Errorf("Incorrect name or password. error: %v", err)
-		return "", "", err
-	}
-
-	if !utils.CheckPasswordHash(user.Password, foundUser.Password) {
-		return "", "", errors.ErrPasswordIncorrect.Error()
-	}
-
-	token, refreshToken, err := utils.TokenGenerator(foundUser.Email, foundUser.FirstName, foundUser.LastName, string(foundUser.ID))
-	if err != nil {
-		log.GetLog().Errorf("Unable to generate tokens. error: %v", err)
-	}
-
-	utils.UpdateAllTokens(u.Postgres, token, refreshToken, string(foundUser.ID))
-	if err != nil {
-		log.GetLog().Errorf("Unable to update tokens. error: %v", err)
-	}
-
-	err = u.TokenRepo.Create(ctx, token, refreshToken, user.ID, time.Now())
-	if err != nil {
-		log.GetLog().Errorf("Unable to create token. error: %v", err)
-		return "", "", err
-	}
-
-	return token, refreshToken, nil
-}
-
 func (u UserHandler) SignUp(ctx context.Context, user *models.User) error {
 	user.ID = rand.Int31()
 
@@ -161,6 +85,36 @@ func (u UserHandler) SignUp(ctx context.Context, user *models.User) error {
 	return nil
 }
 
+func (u UserHandler) Login(ctx context.Context, user *models.User) (string, string, error) {
+	foundUser, err := u.UserRepo.GetByEmail(ctx, user.Email)
+	if err != nil {
+		log.GetLog().Errorf("Incorrect name or password. error: %v", err)
+		return "", "", err
+	}
+
+	if !utils.CheckPasswordHash(user.Password, foundUser.Password) {
+		return "", "", errors.ErrPasswordIncorrect.Error()
+	}
+
+	token, refreshToken, err := utils.TokenGenerator(foundUser.Email, foundUser.FirstName, foundUser.LastName, string(foundUser.ID))
+	if err != nil {
+		log.GetLog().Errorf("Unable to generate tokens. error: %v", err)
+	}
+
+	utils.UpdateAllTokens(u.Postgres, token, refreshToken, string(foundUser.ID))
+	if err != nil {
+		log.GetLog().Errorf("Unable to update tokens. error: %v", err)
+	}
+
+	err = u.TokenRepo.Create(ctx, token, refreshToken, user.ID, time.Now())
+	if err != nil {
+		log.GetLog().Errorf("Unable to create token. error: %v", err)
+		return "", "", err
+	}
+
+	return token, refreshToken, nil
+}
+
 func (u UserHandler) VerifyEmail(ctx context.Context, email string) error {
 	decryptedEmail, err := utils.Decrypt(email)
 	if err != nil {
@@ -175,4 +129,66 @@ func (u UserHandler) VerifyEmail(ctx context.Context, email string) error {
 	}
 
 	return nil
+}
+
+func (u UserHandler) ForgetPassword(ctx context.Context, user *models.User) error {
+	// Find the user based on email
+	foundUser, err := u.UserRepo.GetByEmail(ctx, user.Email)
+	if err != nil {
+		log.GetLog().Errorf("Email does not exist. error: %v", err)
+		return err
+	}
+
+	// Generate a random password
+	newPassword := utils.GenerateRandomPassword(passwordLength)
+
+	// Update user's password with the new random password
+	hashedPassword, err := utils.HashPassword(newPassword)
+	if err != nil {
+		log.GetLog().Errorf("Unable to hash password. error: %v", err)
+		return err
+	}
+	foundUser.Password = hashedPassword
+
+	// Save the updated password to the database
+	if err := u.UserRepo.UpdatePassword(ctx, foundUser.ID, foundUser.Password); err != nil {
+		log.GetLog().Errorf("Unable to update user's password. error: %v", err)
+		return err
+	}
+
+	// Send email with the new password to the user
+	emailBody := fmt.Sprintf(`Hello %s,<br><br>
+	A new password has been requested for your Barista account associated with %s.<br><br>
+
+	Here is your new password: <strong>%s</strong><br><br>
+
+	You can use your random generated password to login to your account.<br><br>
+
+	After logging in your account, you can reset your password in your profile section.<br><br>
+
+	Yours,<br>
+	The Synapse team`, foundUser.FirstName, foundUser.Email, newPassword)
+	err = utils.SendEmail(foundUser.Email, "Barista account recovery", emailBody)
+	if err != nil {
+		log.GetLog().Errorf("Unable to send email. error: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (u UserHandler) UserProfile(ctx context.Context, incoming_token string) (*models.User, error) {
+	userID, err := u.TokenRepo.GetIDByTokenString(ctx, incoming_token)
+	if err != nil {
+		log.GetLog().Errorf("Unable to retrieve user ID via token string")
+		return nil, err
+	}
+
+	user, err := u.UserRepo.GetByID(ctx, userID)
+	if err != nil {
+		log.GetLog().Error(err)
+		return nil, err
+	}
+
+	return user, nil
 }
