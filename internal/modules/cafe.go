e05747e3b8c0c5af7c210d843180ea764972e7d9
mobinbr <mobinbr99@gmail.com>
2024-04-27 11:39:18 +0330

    feat(cafe): Adds & Gets comments

--- internal/modules/cafe.go
+++ internal/modules/cafe.go
@@ -5,13 +5,20 @@ import (
 	"barista/pkg/models"
 	"barista/pkg/repo"
 	"context"
+	"math/rand"
 	"strconv"
+	"time"
+)
+
+const (
+	commentsLimit = 5
 )
 
 type CafeHandler struct {
-	CafeRepo  repo.CafesRepo
-	Rating    repo.RatingsRepo
-	ImageRepo repo.ImageRepo
+	CafeRepo    repo.CafesRepo
+	Rating      repo.RatingsRepo
+	CommentRepo repo.CommentsRepo
+	ImageRepo   repo.ImageRepo
 }
 
 func (c CafeHandler) Create(ctx context.Context, cafe *models.Cafe) error {
@@ -50,8 +57,19 @@ func (c CafeHandler) SearchCafe(ctx context.Context, name string, address string
 			log.GetLog().Errorf("Unable to get cafe images. error: %v", err)
 			continue
 		}
+
+		comments, err := c.CommentRepo.GetLast(ctx, cafes[i].ID, commentsLimit, 0)
+		if err != nil {
+			log.GetLog().Errorf("Unable to get cafe comments. error: %v", err)
+			continue
+		}
+
 		for _, image := range images {
 			cafes[i].Images = append(cafes[i].Images, image.ID)
+		}
+
+		for _, comment := range comments {
+			cafes[i].Comments = append(cafes[i].Comments, *comment)
 		}
 	}
 
@@ -72,4 +90,39 @@ func (c CafeHandler) PublicCafeProfile(ctx context.Context, cafeID string) (*mod
 	}
 
 	return cafe, nil
+}
+
+func (c CafeHandler) AddComment(ctx context.Context, cafeID int32, userID string, comment string) error {
+	user_id, err := strconv.Atoi(userID)
+	if err != nil {
+		log.GetLog().Errorf("Unable to convert user id to int32. error: %v", err)
+		return err
+	}
+
+	commentID := rand.Int31()
+
+	err = c.CommentRepo.Create(ctx, &models.Comment{
+		ID:      commentID,
+		UserID:  int32(user_id),
+		CafeID:  cafeID,
+		Comment: comment,
+		Date:    time.Now(),
+	})
+	if err != nil {
+		log.GetLog().Errorf("Unable to add comment. error: %v", err)
+		return err
+	}
+
+	return err
+}
+
+func (c CafeHandler) GetComments(ctx context.Context, cafeID int32, counter int) ([]*models.Comment, error) {
+	offset := (counter - 1) * commentsLimit
+	comments, err := c.CommentRepo.GetLast(ctx, cafeID, commentsLimit, offset)
+	if err != nil {
+		log.GetLog().Errorf("Unable to get 5 last comments. error: %v", comments)
+		return nil, err
+	}
+
+	return comments, err
 }
