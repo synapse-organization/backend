d0ee8d6e7f438265369349275174d7200be62785
mobinbr <mobinbr99@gmail.com>
2024-04-27 11:37:04 +0330

    feat: Retrieves last comments

--- pkg/repo/comments.go
+++ pkg/repo/comments.go
@@ -4,6 +4,7 @@ import (
 	"barista/pkg/log"
 	"barista/pkg/models"
 	"context"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -11,6 +12,7 @@ type CommentsRepo interface {
 	Create(ctx context.Context, comment *models.Comment) error
 	GetByID(ctx context.Context, id int32) (*models.Comment, error)
 	GetByCafeID(ctx context.Context, cafeID int32) ([]*models.Comment, error)
+	GetLast(ctx context.Context, cafeID int32, limit int32, offset int) ([]*models.Comment, error)
 }
 
 type CommentsRepoImp struct {
@@ -21,9 +23,10 @@ func NewCommentsRepoImp(postgres *pgxpool.Pool) *CommentsRepoImp {
 	_, err := postgres.Exec(context.Background(),
 		`CREATE TABLE IF NOT EXISTS comments (
 				id INTEGER PRIMARY KEY,
-				cafe_id INTEGER,
 				user_id INTEGER,
+				cafe_id INTEGER,
 				comment TEXT,
+				date	TIMESTAMP,
 				FOREIGN KEY (cafe_id) REFERENCES cafes(id),
 				FOREIGN KEY (user_id) REFERENCES users(id)
 			);`)
@@ -34,7 +37,7 @@ func NewCommentsRepoImp(postgres *pgxpool.Pool) *CommentsRepoImp {
 }
 
 func (c *CommentsRepoImp) Create(ctx context.Context, comment *models.Comment) error {
-	_, err := c.postgres.Exec(ctx, "INSERT INTO comments (id, cafe_id, user_id, comment) VALUES ($1, $2, $3, $4)", comment.ID, comment.CafeID, comment.UserID, comment.Comment)
+	_, err := c.postgres.Exec(ctx, "INSERT INTO comments (id, user_id, cafe_id, comment, date) VALUES ($1, $2, $3, $4, $5)", comment.ID, comment.UserID, comment.CafeID, comment.Comment, comment.Date)
 	if err != nil {
 		log.GetLog().Errorf("Unable to insert comment. error: %v", err)
 	}
@@ -66,5 +69,31 @@ func (c *CommentsRepoImp) GetByCafeID(ctx context.Context, cafeID int32) ([]*mod
 		}
 		comments = append(comments, &comment)
 	}
+	return comments, err
+}
+
+func (c *CommentsRepoImp) GetLast(ctx context.Context, cafeID int32, limit int32, offset int) ([]*models.Comment, error) {
+	rows, err := c.postgres.Query(ctx,
+		`SELECT id, user_id, cafe_id, comment, date 
+		FROM comments
+		WHERE cafe_id = $1
+		ORDER BY date DESC
+		LIMIT $2
+		OFFSET $3`, cafeID, limit, offset)
+	if err != nil {
+		log.GetLog().Errorf("Unable to get last %v comments. error: %v", limit, err)
+	}
+	defer rows.Close()
+
+	var comments []*models.Comment
+	for rows.Next() {
+		var comment models.Comment
+		err := rows.Scan(&comment.ID, &comment.UserID, &comment.CafeID, &comment.Comment, &comment.Date)
+		if err != nil {
+			log.GetLog().Errorf("Unable to scan comments. error: %v", err)
+		}
+		comments = append(comments, &comment)
+	}
+
 	return comments, err
 }
