50cb5655b4689c12542489e868a062f5e2ca3c17
mobinbr <mobinbr99@gmail.com>
2024-05-02 17:49:26 +0330

    feat: Adds menu item db

--- pkg/models/menu_items.go
+++ pkg/models/menu_items.go
@@ -1,10 +1,11 @@
 package models
 
 type MenuItem struct {
-	ID       int32            `json:"id"`
-	CafeID   int32            `json:"cafe_id"`
-	Name     string           `json:"name"`
-	Price    float64          `json:"price"`
-	Category MenuItemCategory `json:"category"`
-	ImageID  string           `json:"image_id"`
+	ID          int32            `json:"id"`
+	CafeID      int32            `json:"cafe_id"`
+	Name        string           `json:"name"`
+	Price       float64          `json:"price"`
+	Category    MenuItemCategory `json:"category"`
+	Ingredients []string         `json:"ingredients"`
+	ImageID     string           `json:"image_id"`
 }
